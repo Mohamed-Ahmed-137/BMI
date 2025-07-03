3f5e28fbf1d5943a4e9cb2c99abac21127e95540
Mohamed-Ahmed-137 <ma6634@fayoum.edu.eg>
2025-07-03 15:23:30 +0300

    add-suggestion

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let btn = document.getElementById("btn");
 btn.addEventListener("click", function (e) {
   let result = document.getElementById("result");
+  let suggest = document.getElementById("suggest");
   let input1 = document.getElementById("w");
   let input2 = document.getElementById("h");
 
@@ -22,25 +23,43 @@ btn.addEventListener("click", function (e) {
     result.style.color = "red";
   }
   result.innerHTML = `Your BMI is: ${output.bmi} (${output.msg})`;
+  suggest.innerHTML = `${output.suggestion}`;
 });
 
 function clacBMI(w, h) {
   h = h / 100;
-  let bmi = (w / (h * h)).toFixed(2);
-  // console.log(bmi);
+  let bmiValue = w / (h * h);
+  let bmi = bmiValue.toFixed(2);
   let msg = "";
-  if (bmi < 18.5) {
+  let suggestion = "";
+
+  let idealBMI = 22;
+  let idealWeight = idealBMI * (h * h);
+  let diff = (w - idealWeight).toFixed(1);
+
+  if (bmiValue < 18.5) {
     msg = "Underweight";
-  } else if (bmi >= 18.5 && bmi <= 24.99) {
+    suggestion = `You need to gain about ${Math.abs(
+      diff
+    )} Kg to reach a healthy weight.`;
+  } else if (bmiValue >= 18.5 && bmiValue <= 24.99) {
     msg = "Healthy";
-  } else if (bmi >= 25.0 && bmi <= 29.99) {
+    suggestion = "Your weight is already in the healthy range.";
+  } else if (bmiValue >= 25.0 && bmiValue <= 29.99) {
     msg = "Overweight";
-  } else if (bmi >= 30.0) {
+    suggestion = `You need to lose about ${Math.abs(
+      diff
+    )} Kg to reach a healthy weight.`;
+  } else if (bmiValue >= 30.0) {
     msg = "Obesity";
+    suggestion = `You need to lose about ${Math.abs(
+      diff
+    )} Kg to reach a healthy weight.`;
   }
-  //console.log(msg);
+
   return {
     bmi: bmi,
     msg: msg,
+    suggestion: suggestion,
   };
 }
